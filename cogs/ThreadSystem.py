d140ff9c25b62b108cccfeac9d1573e95f4bdad9
_D4rkS0ul_ <83767949+D4rkS0ulMC@users.noreply.github.com>
2022-02-13 18:40:08 -0500

    Deleted !test command

--- cogs/ThreadSystem.py
+++ cogs/ThreadSystem.py
@@ -33,30 +33,6 @@ class ThreadSystem(commands.Cog):
         elif msg != "" and len(returned_string) > 0:
             await msg.edit(returned_string)
 
-    @commands.command()
-    async def test(self, ctx):
-        rip_role = ctx.guild.get_role(rip_role_id)
-        members = [m for m in ctx.guild.members if rip_role in m.roles]
-        member_mentions = []
-        for member in members:
-            member_mentions.append(member.mention)
-        returned_string = ""
-        msg = ""
-
-        for member_mention in member_mentions:
-            if len(returned_string + member_mention) > 2000:
-                if msg == "":
-                    msg = await ctx.send(returned_string)
-                else:
-                    await msg.edit(returned_string)
-                returned_string = member_mention
-            else:
-                returned_string += member_mention + " "
-        if msg == "" and len(returned_string) > 0:
-            await ctx.send(returned_string)
-        elif msg != "" and len(returned_string) > 0:
-            await msg.edit(returned_string)
-
 
 def setup(bot):
     bot.add_cog(ThreadSystem(bot))
