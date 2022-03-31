79a51e10d32fdae8b9454bb7405e08601dd2bd5d
_D4rkS0ul_ <83767949+D4rkS0ulMC@users.noreply.github.com>
2022-03-31 17:42:12 -0400

    Added debug txt

--- Config.py
+++ Config.py
@@ -5,13 +5,13 @@ prefix = "!"
 debug = False
 
 # RIP Role ID
-rip_role_id = 939633923305132182
+rip_role_id = 941942976429559808
 
 # Mod Role ID
 mod_role_id = 933098293146292325
 
 # Allowed Parent Channel IDs
-allowed_parent_channel_ids = [934295373139304448]
+allowed_parent_channel_ids = [934295373139304448, 915333299981934695]
 
 # Allowed Parent Category IDs
 allowed_parent_category_ids = [933075515881951293]
