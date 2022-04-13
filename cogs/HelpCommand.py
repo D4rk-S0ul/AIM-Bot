9a9219339cf99083ffe604fb5fffae402b98ee29
_D4rkS0ul_ <83767949+D4rkS0ulMC@users.noreply.github.com>
2022-04-13 19:25:07 -0400

    Added !help command

---
+++ cogs/HelpCommand.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+from Config import prefix
+
+
+class HelpCommand(commands.Cog):
+
+    def __init__(self, bot):
+        self.client = bot
+
+    @commands.command()
+    async def help(self, ctx):
+        await ctx.send("**Commands:**\r\n"
+                       f"{prefix}addMembers #thread:\r\n"
+                       f"   *Adds member with RIP Role to the #thread*\r\n"
+                       f"{prefix}sendMsg #channel [Message Content]:\r\n"
+                       f"   *Sends a message containing the message content in #channel*\r\n"
+                       f"{prefix}editMsg #channel [Message ID] [Message Content]:\r\n"
+                       f"   *Edits the message associated with the message ID in #channel*")
+
+
+def setup(bot):
+    bot.add_cog(HelpCommand(bot))
