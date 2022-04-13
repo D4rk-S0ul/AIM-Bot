cc71007b2ef1460c7c6958d2a3a6731603fec469
_D4rkS0ul_ <83767949+D4rkS0ulMC@users.noreply.github.com>
2022-04-13 19:29:18 -0400

    Updated !help format

--- cogs/HelpCommand.py
+++ cogs/HelpCommand.py
@@ -10,12 +10,12 @@ class HelpCommand(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         await ctx.send("**Commands:**\r\n"
-                       f"{prefix}addMembers #thread:\r\n"
-                       f"   *Adds member with RIP Role to the #thread*\r\n"
-                       f"{prefix}sendMsg #channel [Message Content]:\r\n"
-                       f"   *Sends a message containing the message content in #channel*\r\n"
-                       f"{prefix}editMsg #channel [Message ID] [Message Content]:\r\n"
-                       f"   *Edits the message associated with the message ID in #channel*")
+                       f"`{prefix}addMembers #thread:`\r\n"
+                       f"   Adds member with RIP Role to the #thread\r\n"
+                       f"`{prefix}sendMsg #channel [Message Content]:`\r\n"
+                       f"   Sends a message containing the message content in #channel\r\n"
+                       f"`{prefix}editMsg #channel [Message ID] [Message Content]:`\r\n"
+                       f"   Edits the message associated with the message ID in #channel")
 
 
 def setup(bot):
