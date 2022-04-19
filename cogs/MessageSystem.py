5b34ddde8c27f842ab4c125fe3742dfe0d6244b9
_D4rkS0ul_ <83767949+D4rkS0ulMC@users.noreply.github.com>
2022-04-18 22:14:43 -0400

    Fixed Enter in !sendMsg

--- cogs/MessageSystem.py
+++ cogs/MessageSystem.py
@@ -9,17 +9,6 @@ class MessageSystem(commands.Cog):
         self.client = bot
 
     @commands.command()
-    async def editMsg(self, ctx, channel: discord.TextChannel, msg_id: int, *msg_content_input):
-        rip_mod_role = ctx.guild.get_role(rip_mod_role_id)
-        rsds_mod_role = ctx.guild.get_role(rsds_mod_role_id)
-        if rip_mod_role not in ctx.author.roles and rsds_mod_role not in ctx.author.roles and ctx.author.id != 672768917885681678:
-            return
-        msg = await channel.fetch_message(msg_id)
-        msg_content = " ".join(msg_content_input)
-        msg_output = msg_content.replace("/n ", "\r\n")
-        await channel.send(msg_output)
-
-    @commands.command()
     async def sendMsg(self, ctx, channel: discord.TextChannel, *msg_content_input):
         rip_mod_role = ctx.guild.get_role(rip_mod_role_id)
         rsds_mod_role = ctx.guild.get_role(rsds_mod_role_id)
@@ -28,6 +17,17 @@ class MessageSystem(commands.Cog):
         msg_content = " ".join(msg_content_input)
         msg_output = msg_content.replace("/n ", "\r\n")
         await channel.send(msg_output)
+
+    @commands.command()
+    async def editMsg(self, ctx, channel: discord.TextChannel, msg_id: int, *msg_content_input):
+        rip_mod_role = ctx.guild.get_role(rip_mod_role_id)
+        rsds_mod_role = ctx.guild.get_role(rsds_mod_role_id)
+        if rip_mod_role not in ctx.author.roles and rsds_mod_role not in ctx.author.roles and ctx.author.id != 672768917885681678:
+            return
+        msg = await channel.fetch_message(msg_id)
+        msg_content = " ".join(msg_content_input)
+        msg_output = msg_content.replace("/n ", "\r\n")
+        await channel.edit(msg_output)
 
 
 def setup(bot):
