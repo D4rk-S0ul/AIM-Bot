50fa1bb5535e3872cdea45cb65c0d71fd6e7796f
_D4rkS0ul_ <83767949+D4rkS0ulMC@users.noreply.github.com>
2022-04-30 11:22:19 -0400

    Added !addMembers for SEA

--- Config.py
+++ Config.py
@@ -7,6 +7,9 @@ debug = False
 # RIP Role ID
 rip_role_id = 939633923305132182
 
+# SEA Role ID
+sea_role_id = 968954879492104322
+
 # RIP Mod Role ID
 rip_mod_role_id = 933098293146292325
 
@@ -17,7 +20,13 @@ sea_mod_role_id = 959183045326405704
 allowed_parent_channel_ids = [934295373139304448]
 
 # Allowed Parent Category IDs
-allowed_parent_category_ids = [933075515881951293]
+allowed_parent_category_ids = [933075515881951293, 959162588099403866, 959188794282758245, 959197239622910023]
+
+# RIP Server ID
+rip_id = 933075515881951292
+
+# SEA Server ID
+sea_id = 959162264081014814
 
 # Time before timeout
 time = 300
