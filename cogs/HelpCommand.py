7752ff98a7c86cbbb6da57fc8c511f7c60d23f81
_D4rkS0ul_ <83767949+D4rkS0ulMC@users.noreply.github.com>
2022-05-02 20:56:19 -0400

    Updated !help

--- cogs/HelpCommand.py
+++ cogs/HelpCommand.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from Config import prefix
+from Config import prefix, rip_id, sea_id
 
 
 class HelpCommand(commands.Cog):
@@ -9,13 +9,20 @@ class HelpCommand(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
+        server = ""
+        if ctx.guild.id == rip_id:
+            server = "RIP"
+        elif ctx.guild.id == sea_id:
+            server = "SEA"
         await ctx.send("**Commands:**\r\n"
                        f"`{prefix}addMembers #thread:`\r\n"
-                       f"   Adds member with RIP Role to the #thread\r\n"
+                       f"   Adds member with {server} Role to the #thread\r\n"
                        f"`{prefix}sendMsg #channel:`\r\n"
                        f"   Sends a message in #channel\r\n"
                        f"`{prefix}editMsg #channel [Message ID]:`\r\n"
-                       f"   Edits the message associated with the message ID in #channel")
+                       f"   Edits the message associated with the message ID in #channel"
+                       f"`{prefix}archive #channel:`\r\n"
+                       f"   Sends a message using the archive format in #channel")
 
 
 def setup(bot):
