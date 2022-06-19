f6ac6f327e405b4e97124da12b95a31053a15259
_D4rkS0ul_ <83767949+D4rkS0ulMC@users.noreply.github.com>
2022-06-19 09:45:13 +0200

    thread_join fix

--- cogs/ThreadSystem.py
+++ cogs/ThreadSystem.py
@@ -5,17 +5,14 @@ from Config import rip_role_id, rip_mod_role_id, allowed_parent_channel_ids, all
     sea_mod_role_id, rip_id, sea_id, sea_role_id, sea_projects_channel_id, sea_projects_message_id
 
 
-async def add_members(thread, ping_id):
+async def add_members(thread):
     await thread.join()
-    if thread.guild.premium_tier == 3:
-        await thread.edit(auto_archive_duration=10080)
-    elif thread.guild.premium_tier == 2:
-        await thread.edit(auto_archive_duration=4320)
-    elif thread.guild.premium_tier == 1:
-        await thread.edit(auto_archive_duration=1440)
+    await thread.edit(auto_archive_duration=10080)
+    ping_id = ping_id_getter(thread)
     ping_role = thread.guild.get_role(ping_id)
     members = [m for m in thread.guild.members if ping_role in m.roles]
     if len(members) == 0:
+        print("Coundn't find any members with the ping role!")
         return
     member_mentions = [member.mention for member in members]
     returned_string = ""
@@ -33,6 +30,30 @@ async def add_members(thread, ping_id):
         await ping_msg.edit(returned_string)
     await ping_msg.delete()
     await thread.send("Successfully added people to the thread and set auto-archive duration to the max!\r\n")
+    if ping_id == rip_role_id:
+        await rip_tasks(thread)
+    elif ping_id == sea_role_id:
+        await sea_tasks(thread)
+
+
+def ping_id_getter(thread):
+    if thread.guild.id == rip_id:
+        return rip_role_id
+    elif thread.guild.id == sea_id:
+        return sea_role_id
+
+
+async def rip_tasks(thread):
+    pass
+
+
+async def sea_tasks(thread):
+    sea_projects_channel = thread.guild.get_channel(sea_projects_channel_id)
+    msg = await sea_projects_channel.fetch_message(sea_projects_message_id)
+    if thread.name in msg.content:
+        return
+    await msg.edit(f"{msg.content}\r\n"
+                   f" - {thread.name}")
 
 
 class ThreadSystem(commands.Cog):
@@ -42,8 +63,8 @@ class ThreadSystem(commands.Cog):
         self.flag = False
 
     @commands.Cog.listener()
-    async def on_thread_join(self, thread_input):
-        if thread_input.category_id not in allowed_parent_category_ids and thread_input.parent_id not in allowed_parent_channel_ids:
+    async def on_thread_join(self, thread):
+        if thread.category_id not in allowed_parent_category_ids and thread.parent_id not in allowed_parent_channel_ids:
             return
         if self.flag:
             self.flag = False
@@ -53,30 +74,16 @@ class ThreadSystem(commands.Cog):
             self.flag = True
             print("Flag activated!")
 
-        sea_projects_channel = thread_input.guild.get_channel(sea_projects_channel_id)
-        msg = await sea_projects_channel.fetch_message(sea_projects_message_id)
-        await msg.edit(f"{msg.content}\r\n"
-                       f" - {thread_input.name}")
-
-        ping = 0
-        if thread_input.guild.id == rip_id:
-            ping = rip_role_id
-        elif thread_input.guild.id == sea_id:
-            ping = sea_role_id
-        await add_members(thread_input, ping)
+        await add_members(thread)
 
     @commands.command()
-    async def addMembers(self, ctx, arg1: discord.Thread):
+    async def addMembers(self, ctx, thread: discord.Thread):
         rip_mod_role = ctx.guild.get_role(rip_mod_role_id)
         sea_mod_role = ctx.guild.get_role(sea_mod_role_id)
         if rip_mod_role not in ctx.author.roles and sea_mod_role not in ctx.author.roles and ctx.author.id != 672768917885681678:
             return
-        ping = 0
-        if ctx.guild.id == rip_id:
-            ping = rip_role_id
-        elif ctx.guild.id == sea_id:
-            ping = sea_role_id
-        await add_members(arg1, ping)
+
+        await add_members(thread)
 
 
 def setup(bot):
