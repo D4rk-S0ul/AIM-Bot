70142ff33c75fbf0eb14c7e72790fe390c6f4c7e
_D4rkS0ul_ <83767949+D4rkS0ulMC@users.noreply.github.com>
2022-06-27 18:05:50 +0200

    Updated to AIM

--- DiscordBot.py
+++ DiscordBot.py
@@ -13,5 +13,5 @@ for filename in os.listdir('cogs'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
 
-token = os.environ.get("RIP_TOKEN")
+token = os.environ.get("AIM_TOKEN")
 bot.run(token)
