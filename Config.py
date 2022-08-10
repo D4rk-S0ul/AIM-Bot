784bb46097e02ef1a2afb73b8fbcbe1d2bbef9ba
D4rkS0ulMC <bbittrich0@gmail.com>
2022-08-10 23:16:17 +0200

    Added more tags

--- Config.py
+++ Config.py
@@ -36,15 +36,27 @@ time = 300
 
 # Tags
 tags = {
+    "bastionRouteSpreadsheet": "<https://docs.google.com/spreadsheets/d/1qLgp5uhMOKuerNZaec1dpoECpJI0-6YhztMqa_wZ8W0/edit?usp=sharing>",
+    "blazeFight": "<https://youtu.be/dUMclLehKXE>",
     "bridge": "<https://youtu.be/uvvhKX_KnT8>",
+    "cobbleSkip": "<https://youtu.be/HLrsRaij1x8>",
     "dynamicRD": "<https://youtu.be/qfwyFWTY3ds>",
     "housing": "<https://youtu.be/B2SLviws-3c>",
+    "kueeHousing": "<https://www.twitch.tv/pncakespoon/clip/CovertShyTruffleHumbleLife-GbXo9QqoNykzFNLI>",
     "laufCrafting": "<https://youtu.be/OHleXZuhYng>",
     "lavaPlacement": "https://cdn.discordapp.com/attachments/751512715872436416/1005946160386687108/LavaPlacememt.png",
+    "manhuntHousing": "<https://youtu.be/A2tiwLB3DlY>",
+    "mapless": "<https://youtu.be/ujZJw95h0nk>",
     "ninjaBot": "Bot: <https://github.com/Ninjabrain1/Ninjabrain-Bot/releases/>\r\n"
                 "Tutorial: <https://youtu.be/Rx8i7e5lu7g>",
     "ninjabrainBot": "Bot: <https://github.com/Ninjabrain1/Ninjabrain-Bot/releases/>\r\n"
                 "Tutorial: <https://youtu.be/Rx8i7e5lu7g>",
+    "pigPunch": "When you break a chest/gold block, piglins who are on tier 1 **don't** upgrade to tier 2. However, "
+                "when you punch a piglin, piglins **do** upgrade to tier 2, even if they're on tier 1. The "
+                "significance of this is, that piglins on tier 1 lose interest in you as soon as they lose LOS, "
+                "so you want them on tier 2 aggro. Use cases for this are: Manhunt, where you're aggroing piglins "
+                "without armour, bridge manhunt, stables manhunt, treasure bridge, etc. Punching a pig is not "
+                "beneficial in crookst boomer or when you are wearing gold armour.",
     "rawalle": "<https://github.com/joe-ldp/Rawalle/releases/>",
     "resetTracker": "<https://github.com/Specnr/ResetTracker>",
     "rightShoulderAutoFunnel": "https://cdn.discordapp.com/attachments/751512715872436416/1006313251874820257"
@@ -60,5 +72,6 @@ tags = {
                   "/edit>\r\n "
                   "Resource Pack: <https://drive.google.com/file/d/1NXiqmJ40-Oi3LcLQgc8LNlgGhr0TrRG4/view>",
     "wall": "Rawalle: <https://github.com/joe-ldp/Rawalle/releases/>\r\n"
-            "Specnr's wall: <https://github.com/Specnr/MultiResetWall/releases/>"
+            "Specnr's wall: <https://github.com/Specnr/MultiResetWall/releases/>",
+    "woodlight": "<https://youtu.be/QFNvgd32TYY>"
 }
