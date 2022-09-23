074f9207120c75d11714d06e965a92518d8a7936
D4rkS0ulMC <bbittrich0@gmail.com>
2022-09-23 08:32:37 +0200

    migrated to slash command

--- cogs/HelpCommand.py
+++ cogs/HelpCommand.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from Config import prefix, rip_id, sea_id, sea_projects_channel_id
+from discord import slash_command
 
 
 class HelpCommand(commands.Cog):
@@ -7,28 +8,31 @@ class HelpCommand(commands.Cog):
     def __init__(self, bot):
         self.client = bot
 
-    @commands.command()
+    @slash_command(description="Shows a list of commands")
     async def help(self, ctx):
         server = ""
         if ctx.guild.id == rip_id:
             server = "RIP"
         elif ctx.guild.id == sea_id:
             server = "SEA"
-        await ctx.send("**Commands:**\r\n"
-                       f"`{prefix}addMembers #thread:`\r\n"
-                       f"   Adds member with {server} Role to the #thread\r\n"
-                       f"`{prefix}sendMsg #channel:`\r\n"
-                       f"   Sends a message in #channel\r\n"
-                       f"`{prefix}editMsg #channel [Message ID]:`\r\n"
-                       f"   Edits the message associated with the message ID in #channel\r\n"
-                       f"`{prefix}archive #channel:`\r\n"
-                       f"   Sends a message using the archive format in #channel\r\n"
-                       f"`{prefix}editArchive #channel [Message ID]:`\r\n"
-                       f"   Edits the message associated with the message ID in #channel using the archive format\r\n"
-                       f"`{prefix}addProject [Project]:`\r\n"
-                       f"   Adds the project to the <#{sea_projects_channel_id}> (SEA only)\r\n"
-                       f"`{prefix}removeProject [ProjectNumber]:`\r\n"
-                       f"   Removes the object with the corresponding number from the <#{sea_projects_channel_id}>\r\n")
+        await ctx.responde("**Commands:**\r\n"
+                           f"`{prefix}addMembers #thread:`\r\n"
+                           f"   Adds member with {server} Role to the #thread\r\n"
+                           f"`{prefix}sendMsg #channel:`\r\n"
+                           f"   Sends a message in #channel\r\n"
+                           f"`{prefix}editMsg #channel [Message ID]:`\r\n"
+                           f"   Edits the message associated with the message ID in #channel\r\n"
+                           f"`{prefix}archive #channel:`\r\n"
+                           f"   Sends a message using the archive format in #channel\r\n"
+                           f"`{prefix}editArchive #channel [Message ID]:`\r\n"
+                           f"   Edits the message associated with the message ID in #channel using the archive format\r\n"
+                           f"`{prefix}addProject [Project]:`\r\n"
+                           f"   Adds the project to the <#{sea_projects_channel_id}> (SEA only)\r\n"
+                           f"`{prefix}removeProject [ProjectNumber]:`\r\n"
+                           f"   Removes the object with the corresponding number from the <#{sea_projects_channel_id}>\r\n"
+                           "</pin:0>:\r\n"
+                           "   Pins the selected message",
+                           ephemeral=True)
 
 
 def setup(bot):
