b97c0e54fb08c066aaf004cb59b81bb6cedf6aea
D4rkS0ulMC <bbittrich0@gmail.com>
2022-11-20 00:07:54 +0100

    Changed command description

--- cogs/MiscTasks.py
+++ cogs/MiscTasks.py
@@ -9,14 +9,9 @@ class MiscTasks(commands.Cog):
     def __init__(self, bot):
         self.client = bot
 
-    @slash_command(name="pin", description="Pins the message specified!")
-    async def slash_pin(self, ctx,
-                        message: Option(discord.Message, "Enter the message link or ID!", required=True)):
-        await message.pin()
-        await ctx.respond("Pinned message succesfully!", ephemeral=True)
-
-    @message_command(name="Pin Message")
-    async def msg_pin(self, ctx, message: discord.Message):
+    @slash_command(description="Pins the message specified!")
+    async def pin(self, ctx,
+                        message: Option(discord.Message, "Please enter the message link or ID!", required=True)):
         await message.pin()
         await ctx.respond("Pinned message succesfully!", ephemeral=True)
 
