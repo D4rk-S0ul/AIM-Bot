6045c34f6c395eb365b7d36a87de123374e94da9
D4rkS0ulMC <bbittrich0@gmail.com>
2022-11-22 22:00:53 +0100

    removed unnecessary import, fixed typo

--- cogs/MiscTasks.py
+++ cogs/MiscTasks.py
@@ -1,7 +1,7 @@
 import discord
 from discord import Option
 from discord.ext import commands
-from discord.commands import slash_command, message_command
+from discord.commands import slash_command
 
 
 class MiscTasks(commands.Cog):
@@ -11,9 +11,9 @@ class MiscTasks(commands.Cog):
 
     @slash_command(description="Pins the message specified!")
     async def pin(self, ctx,
-                        message: Option(discord.Message, "Please enter the message link or ID!", required=True)):
+                  message: Option(discord.Message, "Please enter the message link or ID!", required=True)):
         await message.pin()
-        await ctx.respond("Pinned message succesfully!", ephemeral=True)
+        await ctx.respond("Pinned message successfully!", ephemeral=True)
 
 
 def setup(bot):
