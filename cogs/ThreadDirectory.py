454b5f9b222b14513b949685947c614fd6a9d4ec
D4rkS0ulMC <bbittrich0@gmail.com>
2022-11-27 13:24:20 +0100

    Revert "commit"
    This reverts commit 95aba38df277bdef756881c5e70691e74b919053.

--- cogs/ThreadDirectory.py
+++ cogs/ThreadDirectory.py
@@ -5,28 +5,27 @@ from discord.ext import commands
 from Config import *
 
 
-def server_getter(ctx):
-    if ctx.guild.id == sea_id:
+def server_getter(thread):
+    if thread.guild.id == sea_id:
         return "SEA"
-    if ctx.guild.id == ear_id:
+    if thread.guild.id == ear_id:
         return "EAR"
     return None
 
 
-def thread_dir_message_getter(server, ctx):
+def thread_dir_message_getter(server, thread):
     thread_dir_message = None
     if server == "SEA":
-        print(ctx.guild)
-        thread_dir_channel = ctx.guild.get_channel(sea_thread_dir_channel_id)
+        thread_dir_channel = thread.guild.get_channel(sea_thread_dir_channel_id)
         thread_dir_message = thread_dir_channel.fetch_message(sea_thread_dir_message_id)
     elif server == "EAR":
-        thread_dir_channel = ctx.guild.get_channel(ear_thread_dir_channel_id)
+        thread_dir_channel = thread.guild.get_channel(ear_thread_dir_channel_id)
         thread_dir_message = thread_dir_channel.fetch_message(ear_thread_dir_message_id)
     return thread_dir_message
 
 
-async def add_thread(server, thread, ctx):
-    thread_dir_message = thread_dir_message_getter(server, ctx)
+async def add_thread(server, thread):
+    thread_dir_message = thread_dir_message_getter(server, thread)
     if thread.name in thread_dir_message.content:
         return
     await thread_dir_message.edit(f"{thread_dir_message.content}\r\n"
@@ -64,11 +63,11 @@ class ThreadDirectory(commands.Cog):
     @thread_dir_group.command(name="add", description="Adds a thread to the thread thread directory (SEA & EAR only)!")
     async def thread_add(self, ctx,
                          thread: Option(str, "Please enter the name of the project!", required=True)):
-        server = server_getter(ctx)
+        server = server_getter(thread)
         if server is None:
             await ctx.respond(f"Unknown Server!", ephemeral=True)
             return
-        add_thread(server, thread, ctx)
+        add_thread(server, thread)
         await ctx.respond(f'Successfully added the thread "{thread}" to the thread directory!', ephemeral=True)
 
     @thread_dir_group.command(name="remove", description="Removes a thread from the thread directory (SEA & EAR only)!")
