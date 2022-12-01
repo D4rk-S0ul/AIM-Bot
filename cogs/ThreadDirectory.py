800bcdedd1f45071236fe4ae703294b73aa33326
D4rkS0ulMC <bbittrich0@gmail.com>
2022-12-01 21:59:47 +0100

    Updated SEA thread_dir message id

--- cogs/ThreadDirectory.py
+++ cogs/ThreadDirectory.py
@@ -2,7 +2,7 @@ import discord
 from discord import Option, SlashCommandGroup
 from discord.ext import commands
 
-from Config import sea_id, sea_projects_channel_id, sea_projects_message_id
+from Config import sea_id, sea_thread_dir_channel_id, sea_thread_dir_message_id
 
 
 class ThreadDirectory(commands.Cog):
@@ -20,8 +20,8 @@ class ThreadDirectory(commands.Cog):
     @thread_dir_group.command(name="add", description="Adds a thread to the thread directory!")
     async def project_add(self, ctx,
                           thread: Option(discord.Thread, "Please enter a thread!", required=True)):
-        sea_projects_channel = ctx.guild.get_channel(sea_projects_channel_id)
-        msg = await sea_projects_channel.fetch_message(sea_projects_message_id)
+        sea_projects_channel = ctx.guild.get_channel(sea_thread_dir_channel_id)
+        msg = await sea_projects_channel.fetch_message(sea_thread_dir_message_id)
         await msg.edit(f"{msg.content}\r\n"
                        f" - <#{thread.id}>")
         await ctx.respond(f'Successfully added the thread "{thread}" to the thread directory!', ephemeral=True)
@@ -29,8 +29,8 @@ class ThreadDirectory(commands.Cog):
     @thread_dir_group.command(name="remove", description="Removes a thread from the thread directory!")
     async def project_remove(self, ctx,
                              thread: Option(discord.Thread, "Please enter thread to be removed!", required=True)):
-        sea_projects_channel = ctx.guild.get_channel(sea_projects_channel_id)
-        msg = await sea_projects_channel.fetch_message(sea_projects_message_id)
+        sea_projects_channel = ctx.guild.get_channel(sea_thread_dir_channel_id)
+        msg = await sea_projects_channel.fetch_message(sea_thread_dir_message_id)
         project_list = msg.content.splitlines()
         updated_project_list = [project for project in project_list if f"<#{thread.id}>" not in project]
         updated_msg = '\r\n'.join(updated_project_list)
