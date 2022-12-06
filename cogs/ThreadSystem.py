f6ce7bd850bcb69fd787332e294152afbb8418f4
D4rkS0ulMC <bbittrich0@gmail.com>
2022-12-06 17:55:30 +0100

    New threads will now be added to the thread dir

--- cogs/ThreadSystem.py
+++ cogs/ThreadSystem.py
@@ -4,7 +4,8 @@ from discord.commands import slash_command
 from discord.ext import commands
 
 from Config import rip_id, sea_id, ear_id, rip_role_id, sea_role_id, ear_role_id, blocked_parent_category_ids, \
-    sea_thread_dir_channel_id, sea_thread_dir_message_id, ear_thread_dir_channel_id, ear_thread_dir_message_id
+    sea_thread_dir_channel_id, sea_thread_dir_message_id, ear_thread_dir_channel_id, ear_thread_dir_message_id, \
+    test_role_id, test_id, test_thread_dir_channel_id, test_thread_dir_message_id
 
 
 def is_allowed_thread(thread):
@@ -22,6 +23,8 @@ def get_server(thread):
         return "SEA"
     if thread.guild.id == ear_id:
         return "EAR"
+    if thread.guild.id == test_id:
+        return "TEST"
     return None
 
 
@@ -32,6 +35,8 @@ def get_ping_role(server, thread):
         return thread.guild.get_role(sea_role_id)
     if server == "EAR":
         return thread.guild.get_role(ear_role_id)
+    if server == "TEST":
+        return thread.guild.get_role(test_role_id)
     return None
 
 
@@ -82,6 +87,8 @@ async def add_members(thread):
         await sea_tasks(thread)
     elif server == "EAR":
         await ear_tasks(thread)
+    elif server == "TEST":
+        await test_tasks(thread)
 
     return failed, msg
 
@@ -91,12 +98,11 @@ async def rip_tasks(thread):
 
 
 async def sea_tasks(thread):
-    sea_projects_channel = thread.guild.get_channel(sea_thread_dir_channel_id)
-    msg = await sea_projects_channel.fetch_message(sea_thread_dir_message_id)
+    thread_dir_channel = thread.guild.get_channel(sea_thread_dir_channel_id)
+    msg = await thread_dir_channel.fetch_message(sea_thread_dir_message_id)
     if thread.id in msg.content:
         return
-    await msg.edit(f"{msg.content}\r\n"
-                   f" - <#{thread.id}>")
+    await add_thread(msg, thread)
 
 
 async def ear_tasks(thread):
@@ -104,8 +110,43 @@ async def ear_tasks(thread):
     msg = await ear_projects_channel.fetch_message(ear_thread_dir_message_id)
     if thread.id in msg.content:
         return
-    await msg.edit(f"{msg.content}\r\n"
-                   f" - <#{thread.id}>")
+    await add_thread(msg, thread)
+
+async def test_tasks(thread):
+    thread_dir_channel = thread.guild.get_channel(test_thread_dir_channel_id)
+    msg = await thread_dir_channel.fetch_message(test_thread_dir_message_id)
+    if str(thread.id) in msg.content:
+        return
+    await add_thread(msg, thread)
+
+async def add_thread(msg, thread):
+    msg_content = msg.content.splitlines()
+    thread_ids = [line[5:-1] for line in msg_content if line.startswith(" - <#")]
+    thread_ids.append(thread.id)
+    parent_ids = await get_parent_ids(thread_ids, thread)
+    msg_parts = await get_message_parts(parent_ids, thread_ids, thread)
+    updated_msg = '\r\n'.join(msg_parts)
+    await msg.edit(updated_msg)
+
+
+async def get_parent_ids(thread_ids, thread_ctx):
+    parent_ids = []
+    for thread_id in thread_ids:
+        thread = await thread_ctx.guild.fetch_channel(thread_id)
+        if thread.parent_id not in parent_ids:
+            parent_ids.append(thread.parent.id)
+    return parent_ids
+
+
+async def get_message_parts(parent_ids, thread_ids, thread_ctx):
+    msg_parts = ["**Thread Directory:**"]
+    for parent_id in parent_ids:
+        msg_parts.append(f"\r\n<#{parent_id}>:")
+        for thread_id in thread_ids:
+            thread = await thread_ctx.guild.fetch_channel(thread_id)
+            if thread.parent.id == parent_id:
+                msg_parts.append(f" - <#{thread_id}>")
+    return msg_parts
 
 
 class ThreadSystem(commands.Cog):
