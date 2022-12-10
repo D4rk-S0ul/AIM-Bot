9e4bfeed79005db4b3be9c75f9e7079f51b3722d
D4rkS0ulMC <bbittrich0@gmail.com>
2022-12-10 11:59:20 +0100

    Removed presence intend

--- DiscordBot.py
+++ DiscordBot.py
@@ -5,12 +5,16 @@ from discord.ext import commands
 
 from Config import prefix
 
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix),
-                   intents=discord.Intents.all(), help_command=None)
+                   intents=intents, help_command=None)
 
 for filename in os.listdir('cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-token = os.environ.get("AIM_TOKEN")
+token = os.environ.get("TEST_TOKEN")
 bot.run(token)
