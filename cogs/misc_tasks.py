235829836d7df57cff6e401e7d4863dfd79b144f
D4rkS0ulMC <bbittrich0@gmail.com>
2023-02-18 21:10:54 +0100

    Added debug msgs

--- cogs/misc_tasks.py
+++ cogs/misc_tasks.py
@@ -1,6 +1,10 @@
+import logging
+
 import discord
 from discord.ext import commands
 
+# Getting logger
+logger = logging.getLogger("discord_bot")
 
 class MiscTasks(commands.Cog):
 
@@ -14,13 +18,17 @@ class MiscTasks(commands.Cog):
                   ):
         if channel is None:
             channel = ctx.channel
+        logger.debug(f"Pinning message in #{channel}...")
         msg = await channel.fetch_message(int(msg_id))
         await msg.pin()
         await ctx.respond("Pinned message successfully!", ephemeral=True)
+        logger.debug(f"Pinned message in #{channel}!")
 
     @commands.slash_command(description="Shows the bot's latency!")
     async def ping(self, ctx: discord.commands.context.ApplicationContext):
+        logger.debug(f"Sending pong...")
         await ctx.respond(f"Ping: {round(self.client.latency * 1000)}ms", ephemeral=True)
+        logger.debug(f"Sent pong! (Ping: {round(self.client.latency * 1000)}ms)")
 
 
 def setup(bot):
