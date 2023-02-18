21eb8c6d5a6b7b629bd46b0437274af631b4e4ac
D4rkS0ulMC <bbittrich0@gmail.com>
2023-02-18 20:28:53 +0100

    Added ping command

--- cogs/misc_tasks.py
+++ cogs/misc_tasks.py
@@ -8,7 +8,7 @@ class MiscTasks(commands.Cog):
         self.client = bot
 
     @commands.slash_command(description="Pins the message specified!")
-    async def pin(self, ctx,
+    async def pin(self, ctx: discord.commands.context.ApplicationContext,
                   msg_id: discord.Option(str, "Please enter the message ID!", required=True),
                   channel: discord.Option(discord.TextChannel, "Please enter the channel!", required=False)
                   ):
@@ -17,6 +17,10 @@ class MiscTasks(commands.Cog):
         msg = await channel.fetch_message(int(msg_id))
         await msg.pin()
         await ctx.respond("Pinned message successfully!", ephemeral=True)
+
+    @commands.slash_command(description="Shows the bot's latency!")
+    async def ping(self, ctx: discord.commands.context.ApplicationContext):
+        await ctx.respond(f"Ping: {round(self.client.latency * 1000)}ms", ephemeral=True)
 
 
 def setup(bot):
