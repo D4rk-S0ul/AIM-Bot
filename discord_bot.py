a1a81d8662e0419059dd21901973922afad6394b
D4rkS0ulMC <bbittrich0@gmail.com>
2023-02-18 14:27:23 +0100

    Double quotes

--- discord_bot.py
+++ discord_bot.py
@@ -6,11 +6,11 @@ from discord.ext import commands
 import config
 
 # Logging setup
-logger = logging.getLogger('discord_bot')
+logger = logging.getLogger("discord_bot")
 logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler('log.log', mode='w')
-formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s", datefmt="%d.%m.%Y: %I:%M:%S %p')
+file_handler = logging.FileHandler("log.log", mode="w")
+formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s", datefmt="%d.%m.%Y: %I:%M:%S %p")
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
@@ -19,21 +19,21 @@ logger.addHandler(file_handler)
 bot = commands.Bot(intents=config.intents, help_command=None)
 
 # Load cogs
-logger.debug('Loading cogs...')
-for filename in os.listdir('cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-        logger.debug(f'Loaded {filename} successfully!')
+logger.debug("Loading cogs...")
+for filename in os.listdir("cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+        logger.debug(f"Loaded {filename} successfully!")
 
 
 # On ready
 @bot.event
 async def on_ready():
-    msg = f'{bot.user.name} is online now! BotID: {bot.user.id}'
-    print(f'\n\n{msg}\n\n')
+    msg = f"{bot.user.name} is online now! BotID: {bot.user.id}"
+    print(f"\n\n{msg}\n\n")
     logger.info(msg)
 
 
 # Run bot
-token = os.environ.get('AIM_TOKEN')
+token = os.environ.get("AIM_TOKEN")
 bot.run(token)
