d3273cece51c177d3776b32c1daec68072735aa7
D4rkS0ulMC <bbittrich0@gmail.com>
2023-02-20 13:23:55 +0100

    Added documentation

--- cogs/threads.py
+++ cogs/threads.py
@@ -1,3 +1,5 @@
+import logging
+
 import discord
 from discord import Option
 from discord.commands import slash_command
@@ -5,81 +7,64 @@ from discord.ext import commands
 
 import functions
 
-
-async def add_members(thread):
-    failed = False
-    msg = None
-    await thread.join()
-    await thread.edit(auto_archive_duration=10080)
-
-    server = functions.get_server(thread)
-    if server is None:
-        failed = True
-        msg = "Unknown server!"
-        return failed, msg
-
-    ping_role = await functions.get_ping_role(server, thread)
-    if ping_role is None:
-        failed = True
-        msg = "No ping role found!"
-        return failed, msg
-
-    members = [member for member in thread.guild.members if ping_role in member.roles]
-    if len(members) == 0:
-        failed = True
-        msg = "Couldn't find any members with the ping role!"
-        return failed, msg
-    member_mentions = [member.mention for member in members]
-
-    returned_string = ""
-    ping_msg = await thread.send("Adding users...")
-    counter = 0
-    for member_mention in member_mentions:
-        if len(returned_string + member_mention) > 2000 or counter == 10:
-            await ping_msg.edit(returned_string)
-            returned_string = member_mention + " "
-            counter = 1
-        else:
-            returned_string += member_mention + " "
-            counter += 1
-    if len(returned_string) != 0:
-        await ping_msg.edit(returned_string)
-    await ping_msg.delete()
-    await thread.send("Successfully added people to the thread and set auto-archive duration to the max!\r\n")
-
-    await functions.add_thread(server, thread)
-
-    return failed, msg
+# Getting logger
+logger = logging.getLogger("discord_bot")
 
 
 class ThreadSystem(commands.Cog):
+    """Cog for the thread system."""
 
     def __init__(self, bot):
+        """Initializes the cog.
+
+        Parameters
+        ------------
+        bot: commands.Bot
+            The bot instance."""
         self.client = bot
         self.flag = False
 
     @commands.Cog.listener()
-    async def on_thread_create(self, thread):
+    async def on_thread_create(self, thread: discord.Thread):
+        """Event triggered when a thread is created.
+
+        Parameters
+        ------------
+        thread: discord.Thread
+            The thread that was created."""
+        logger.debug(f"Thread {thread.id} was created!")
         if not functions.is_allowed_thread(thread):
             return
 
-        await add_members(thread)
+        await functions.add_members(thread)
 
     @slash_command(name="add_members", description="Adds the members to the thread specified!")
-    async def addMembers(self, ctx,
+    async def addMembers(self, ctx: discord.ApplicationContext,
                          thread: Option(discord.Thread, "Discord Thread", required=False)):
+        """Command for adding members to a thread.
+
+        Parameters
+        ------------
+        ctx: commands.ApplicationContext
+            The context used for command invocation.
+        thread: discord.Thread
+            The thread to add members to."""
         if thread is None:
             thread = ctx.channel
+        logger.debug(f"Adding members to thread {thread.id}...")
         if not functions.is_allowed_thread(thread):
             await ctx.respond("Not allowed to execute this command in the specified thread.", ephemeral=True)
+            logger.debug("Not allowed to execute this command in the specified thread.")
             return
         await ctx.defer(ephemeral=True)
-        failed, fail_msg = await add_members(thread)
+        failed, fail_msg = await functions.add_members(thread)
         if failed:
             await ctx.followup.send(fail_msg)
             return
         await ctx.followup.send("Added members successfully!")
+        logger.debug("Added members successfully!")
 
 
 def setup(bot):
+    """Function that is called when the cog is loaded. Adds the cog to the bot."""
     bot.add_cog(ThreadSystem(bot))
--- functions.py
+++ functions.py
@@ -103,3 +103,49 @@ def is_allowed_thread(thread) -> bool:
     if thread.parent.category_id in config.blocked_parent_category_ids:
         return False
     return True
+
+
+async def add_members(thread) -> tuple:
+    failed = False
+    msg = None
+    await thread.join()
+    await thread.edit(auto_archive_duration=10080)
+
+    server = config.servers.get(thread.guild.id)
+    if server is None:
+        failed = True
+        msg = "Unknown server!"
+        return failed, msg
+
+    ping_role = await get_ping_role(server, thread)
+    if ping_role is None:
+        failed = True
+        msg = "No ping role found!"
+        return failed, msg
+
+    members = [member for member in thread.guild.members if ping_role in member.roles]
+    if len(members) == 0:
+        failed = True
+        msg = "Couldn't find any members with the ping role!"
+        return failed, msg
+    member_mentions = [member.mention for member in members]
+
+    returned_string = ""
+    ping_msg = await thread.send("Adding users...")
+    counter = 0
+    for member_mention in member_mentions:
+        if len(returned_string + member_mention) > 2000 or counter == 10:
+            await ping_msg.edit(returned_string)
+            returned_string = member_mention + " "
+            counter = 1
+        else:
+            returned_string += member_mention + " "
+            counter += 1
+    if len(returned_string) != 0:
+        await ping_msg.edit(returned_string)
+    await ping_msg.delete()
+    await thread.send("Successfully added people to the thread and set auto-archive duration to the max!\r\n")
+
+    await add_thread(server, thread)
+
+    return failed, msg
