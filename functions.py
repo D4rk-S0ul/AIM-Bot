b94072e359bc86b1ae1b704264c753b1b30c7f34
D4rkS0ulMC <bbittrich0@gmail.com>
2023-02-20 13:15:32 +0100

    Added documentation

--- functions.py
+++ functions.py
@@ -1,26 +1,46 @@
 import discord
 
-from config import servers, ping_role_ids, thread_dirs, blocked_parent_category_ids
+import config
 
 
-def get_server(ctx_or_thread):
-    return servers.get(ctx_or_thread.guild.id)
+async def get_ping_role(server, ctx_or_thread) -> discord.Role:
+    """Function for getting the ping role.
 
-
-async def get_ping_role(server, ctx_or_thread):
-    ping_role_id = ping_role_ids.get(server)
+    Parameters
+    ------------
+    server: str
+        The server the ping role is in.
+    ctx_or_thread: commands.ApplicationContext or discord.Thread
+        The context or thread used for command invocation."""
+    ping_role_id = config.ping_role_ids.get(server)
     ping_role = ctx_or_thread.guild.get_role(ping_role_id)
     return ping_role
 
 
-async def get_thread_dir_msg(server, ctx_or_thread):
-    channel_id, message_id = thread_dirs.get(server)
+async def get_thread_dir_msg(server, ctx_or_thread) -> discord.Message:
+    """Function for getting the thread directory message.
+
+    Parameters
+    ------------
+    server: str
+        The server the thread directory message is in.
+    ctx_or_thread: commands.ApplicationContext or discord.Thread
+        The context or thread used for command invocation."""
+    channel_id, message_id = config.thread_dirs.get(server)
     channel = ctx_or_thread.guild.get_channel(channel_id)
     msg = await channel.fetch_message(message_id)
     return msg
 
 
-async def get_parent_ids(thread_ids, ctx_or_thread):
+async def get_parent_ids(thread_ids, ctx_or_thread) -> list:
+    """Function for getting the parent ids of the threads.
+
+    Parameters
+    ------------
+    thread_ids: list
+        The ids of the threads.
+    ctx_or_thread: commands.ApplicationContext or discord.Thread
+        The context or thread used for command invocation."""
     parent_ids = []
     for thread_id in thread_ids:
         thread = await ctx_or_thread.guild.fetch_channel(thread_id)
@@ -29,7 +49,17 @@ async def get_parent_ids(thread_ids, ctx_or_thread):
     return parent_ids
 
 
-async def get_message_parts(parent_ids, thread_ids, ctx_or_thread):
+async def get_message_parts(parent_ids, thread_ids, ctx_or_thread) -> list:
+    """Function for getting the message parts of the thread directory message.
+
+    Parameters
+    ------------
+    parent_ids: list
+        The ids of the parent threads.
+    thread_ids: list
+        The ids of the threads.
+    ctx_or_thread: commands.ApplicationContext or discord.Thread
+        The context or thread used for command invocation."""
     msg_parts = ["**Thread Directory:**"]
     for parent_id in parent_ids:
         msg_parts.append(f"\r\n<#{parent_id}>:")
@@ -41,6 +71,14 @@ async def get_message_parts(parent_ids, thread_ids, ctx_or_thread):
 
 
 async def add_thread(server, thread):
+    """Function for adding a thread to the thread directory message.
+
+    Parameters
+    ------------
+    server: str
+        The server the thread is in.
+    thread: discord.Thread
+        The thread to add."""
     msg = await get_thread_dir_msg(server, thread)
     if str(thread.id) in msg.content:
         return
@@ -53,9 +91,15 @@ async def add_thread(server, thread):
     await msg.edit(updated_msg)
 
 
-def is_allowed_thread(thread):
+def is_allowed_thread(thread) -> bool:
+    """Function for checking if a thread is allowed to be added to the thread directory message.
+
+    Parameters
+    ------------
+    thread: discord.Thread
+        The thread to check."""
     if not isinstance(thread, discord.Thread):
         return False
-    if thread.parent.category_id in blocked_parent_category_ids:
+    if thread.parent.category_id in config.blocked_parent_category_ids:
         return False
     return True
