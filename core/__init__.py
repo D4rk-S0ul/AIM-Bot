384b482b66c4047afa02305c3f03289ebe778333
D4rk-S0ul <bbittrich0@gmail.com>
2023-07-09 12:52:12 +0200

    Added discord.Embed subclasses

--- core/__init__.py
+++ core/__init__.py
@@ -1,18 +1,24 @@
 from discord.ext import commands
 
 from .bot import AimBot
+from .embeds import *
 from .utils import *
 
 __all__ = (
     "add_members",
     "add_to_thread_directory",
     "AimBot",
+    "BlurpleEmbed",
     "Cog",
+    "ErrorEmbed",
     "get_permissions",
     "get_tag",
     "get_tutorial_embed",
+    "GreenEmbed",
     "is_valid_thread",
-    "remove_from_thread_directory"
+    "RedEmbed",
+    "remove_from_thread_directory",
+    "SuccessEmbed"
 )
 
 
