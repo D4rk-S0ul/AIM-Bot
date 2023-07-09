4173c8ccc4beb65c5d45207a6380513835673e55
D4rk-S0ul <bbittrich0@gmail.com>
2023-07-09 16:47:58 +0200

    Revert "Added thread.py"
    This reverts commit efb0bc79228592ebc1fdbc93d62e21f473b9dd3a.

--- core/__init__.py
+++ core/__init__.py
@@ -2,7 +2,6 @@ from discord.ext import commands
 
 from .bot import AimBot
 from .utils import *
-from .thread import *
 
 __all__ = (
     "add_members",
@@ -14,8 +13,7 @@ __all__ = (
     "get_tutorial_embed",
     "get_valid_thread",
     "is_valid_thread",
-    "remove_from_thread_directory",
-    "Thread"
+    "remove_from_thread_directory"
 )
 
 
