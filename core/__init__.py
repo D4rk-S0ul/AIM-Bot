5ebc1cec9b81ceea1807387ec909ab78b7ef9621
D4rk-S0ul <bbittrich0@gmail.com>
2023-07-09 13:08:11 +0200

    Revert "Added discord.Embed subclasses"
    This reverts commit 384b482b66c4047afa02305c3f03289ebe778333.

--- core/__init__.py
+++ core/__init__.py
@@ -1,24 +1,18 @@
 from discord.ext import commands
 
 from .bot import AimBot
-from .embeds import *
 from .utils import *
 
 __all__ = (
     "add_members",
     "add_to_thread_directory",
     "AimBot",
-    "BlurpleEmbed",
     "Cog",
-    "ErrorEmbed",
     "get_permissions",
     "get_tag",
     "get_tutorial_embed",
-    "GreenEmbed",
     "is_valid_thread",
-    "RedEmbed",
-    "remove_from_thread_directory",
-    "SuccessEmbed"
+    "remove_from_thread_directory"
 )
 
 
