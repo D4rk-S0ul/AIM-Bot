332a74dd52f9c57602b527d550340747972e6e45
D4rk-S0ul <bbittrich0@gmail.com>
2023-09-22 23:06:37 +0200

    Added subclassing for embeds

--- core/__init__.py
+++ core/__init__.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 
 from .bot import AimBot
 from .config import *
+from .embeds import *
 from .utils import *
 
 __all__ = (
@@ -10,14 +11,22 @@ __all__ = (
     "add_to_thread_directory",
     "AimBot",
     "Cog",
+    "Embed",
+    "ErrorEmbed",
     "get_permissions",
     "get_tag",
     "get_tutorial_embed",
     "get_valid_thread",
+    "HelpEmbed",
+    "HelpSelect",
+    "HelpSelectEmbed",
     "is_feedback",
     "is_valid_thread",
+    "ProgressEmbed",
     "remove_from_feedback_thread_directory",
-    "remove_from_thread_directory"
+    "remove_from_thread_directory",
+    "SuccessEmbed",
+    "WarningEmbed"
 )
 
 
