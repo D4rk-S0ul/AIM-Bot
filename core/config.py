7a675ee8e8380187b44019459214eed16bc71844
D4rk-S0ul <bbittrich0@gmail.com>
2024-01-02 14:01:06 +0100

    Updated to version 2.2.5: Added additional feedback strings vor video Feedback

--- core/config.py
+++ core/config.py
@@ -1,9 +1,10 @@
-version: str = "2.2.4"
+version: str = "2.2.5"
 
 bell_tag_id = 1132640430090113024
 rip_guild_id = 933075515881951292
 feedback_channel_id = 1019760331532275732
 feedback_strings = [
+    "## Feedback",
     "## <:Overworld:1132644632489103371>  Overworld",
     "## <:Nether:1132644630576517190>  Going to Bastion",
     "## <:Bastion:1138327109929025536>  Bastion Split",
@@ -12,5 +13,7 @@ feedback_strings = [
     "## <:Triangulation:1138327300736290906>  Finding/Going to Stronghold"
     "## <:Stronghold:1138327270625378324>  Stronghold Split",
     "## <:End:1132644627506278451>  End Split",
-    "## <:Backseatega:933142144699494410> Reoccurring Themes"
+    "## <:Backseatega:933142144699494410> Reoccurring Themes",
+    "## <:YouTube:1191724962868904037> Feedback",
+    "## <:YouTube:1191724962868904037> Video Feedback",
 ]
